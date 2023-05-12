a76744745df8370422379c353abcd2e58e89af9c
Tran Phong <fong1668vn@gmail.com>
2023-05-13 01:42:00 +0700

    fix cors

--- src/main/java/com/example/WeatherAPI/WeatherApiApplication.java
+++ src/main/java/com/example/WeatherAPI/WeatherApiApplication.java
@@ -2,6 +2,9 @@ package com.example.WeatherAPI;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WeatherApiApplication {
@@ -9,5 +12,4 @@ public class WeatherApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApiApplication.class, args);
 	}
-
 }
